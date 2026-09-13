32e18b13e86b7a562a740d8ef6faa205ec4032a8
hkaliszuk <h.kaliszuk@student.uw.edu.pl>
2026-09-13 18:15:47 +0200

    game result model; constants' file

--- game/models.py
+++ game/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+from django.contrib.auth.models import User
+from .constants import MAX_LEVEL
 
-# Create your models here.
+class GameResult(models.Model):
+    # each result belongs to one user
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    level = models.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(MAX_LEVEL)]
+    )
+    score = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
