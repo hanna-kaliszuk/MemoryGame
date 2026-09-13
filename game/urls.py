2333c1fdc751f23d44bb233fc0db4ec26a69ad4a
hkaliszuk <h.kaliszuk@student.uw.edu.pl>
2026-09-13 18:54:11 +0200

    registration possible

--- game/urls.py
+++ game/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
-    path("accounts/", include("django.contrib.auth.urls"))
+    path("accounts/", include("django.contrib.auth.urls")),
+    path("accounts/register/", views.register, name="register"),
 ]
--- game/views.py
+++ game/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render
-
+from django.contrib.auth import login
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
+
+from .forms import RegisterForm
 
 def home(request):
     return HttpResponse("Memory Game")
+
+def register(request):
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("/")
+    else:
+        form = RegisterForm()
+
+    return render(request, "registration/register.html", {"form": form})
